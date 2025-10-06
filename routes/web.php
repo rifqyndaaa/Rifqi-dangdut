a04fd357b9f06ae85beceefb5cd0121d1f3f59f1
rifqyndaaa <yanda24si@mahasiswa.pcr.ac.id>
2025-10-06 10:02:52 +0700

    halama dasbor volte

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -34,3 +36,5 @@ Route::get('/home', [HomeController::class, 'index']);
 
 Route::post('question/store', [QuestionController::class, 'store'])
     ->name('question.store');
+
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
