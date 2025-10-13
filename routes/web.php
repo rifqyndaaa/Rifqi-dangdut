367b76deecc5676f31db3026eaa8ccb8fb0a24a5
rifqyndaaa <yanda24si@mahasiswa.pcr.ac.id>
2025-10-13 08:32:09 +0700

    membuat create blade

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\QuestionController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 
 Route::get('/', function () {
@@ -38,3 +39,5 @@ Route::post('question/store', [QuestionController::class, 'store'])
     ->name('question.store');
 
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    Route::resource('pelanggan', PelangganController::class);
